a42381d3355d234706a2c1d8c966858b49d8dde4
ASzoltysek <A@ania.home>
2017-12-05 18:07:31 +0100

    day5 part2 completed

---
+++ src/com/adventOfCode/annieq/day5/Part2.java
@@ -0,0 +1,34 @@
+package com.adventOfCode.annieq.day5;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.adventOfCode.annieq.utilities.InputImport;
+
+public class Part2 {
+    
+    public static void main(String[] args) throws IOException {
+        InputImport in = new InputImport("data//day5.txt");
+        ArrayList<String> lines = in.getMultiLineData();        
+        int[] jumps = lines.stream().mapToInt(Integer::parseInt).toArray();
+        
+        int counter = 0; // number of jumps
+        int pointer = 0;
+        int tmp = 0; // temporary pointer
+        try {
+            while (true) {
+                tmp = pointer;
+                pointer += jumps[pointer];
+                if (jumps[tmp] >= 3)
+                    --jumps[tmp];
+                else 
+                    ++jumps[tmp];
+                ++counter;
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println(counter);
+        }
+        
+    }
+    
+}
