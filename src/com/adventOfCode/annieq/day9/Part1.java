57fa842c52673a4856bffdd74ffd9b0f01f785d1
ASzoltysek <A@ania.home>
2017-12-09 13:35:44 +0100

    day9 part1 completed

--- src/com/adventOfCode/annieq/day9/Part1.java
+++ src/com/adventOfCode/annieq/day9/Part1.java
@@ -10,5 +10,44 @@ public class Part1 {
 
         InputImport in = new InputImport("data//day9.txt");
         String stream = in.getSingleLineData();
+        
+        
+        // remove all cancelled characters
+        for (int i = 0; i < stream.length()-1; ++i) {
+            if (stream.charAt(i) == '!') {
+                stream = stream.substring(0, i).concat(stream.substring(i+2));
+                --i;
+            }
+        }
+        
+        // remove garbage
+        for (int i = 0; i < stream.length(); ++i) {
+            if (stream.charAt(i) == '<') {
+                // garbage starts here
+                for (int j = i + 1; j < stream.length(); ++j) {
+                    if (stream.charAt(j) == '>') {
+                        stream = stream.substring(0, i).concat(stream.substring(j+1));
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // count score
+        int total = 0;
+        int currentScore = 0;
+        
+        for (int i = 0; i < stream.length(); ++i) {
+            if (stream.charAt(i) == '{') {
+                ++currentScore;
+                total += currentScore;
+            }
+            if (stream.charAt(i) == '}') {
+                --currentScore;
+            }
+        }
+        
+        System.out.println(total);
     }
+    
 }
