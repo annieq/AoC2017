ff5bcba58ca023ccd8b05ea4a03e774226058ae8
ASzoltysek <A@ania.home>
2017-12-09 12:48:46 +0100

    day9 part1 started

---
+++ src/com/adventOfCode/annieq/day9/Part1.java
@@ -0,0 +1,14 @@
+package com.adventOfCode.annieq.day9;
+
+import java.io.IOException;
+
+import com.adventOfCode.annieq.utilities.InputImport;
+
+public class Part1 {
+
+    public static void main(String[] args) throws IOException {
+
+        InputImport in = new InputImport("data//day9.txt");
+        String stream = in.getSingleLineData();
+    }
+}
