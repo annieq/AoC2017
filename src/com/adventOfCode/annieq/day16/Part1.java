e1f42b93d0fff3daf33afb488d9ad9d423e89fcb
ASzoltysek <A@ania.home>
2017-12-17 22:25:26 +0100

    day16 part1 completed

--- src/com/adventOfCode/annieq/day16/Part1.java
+++ src/com/adventOfCode/annieq/day16/Part1.java
@@ -1,5 +1,42 @@
 package com.adventOfCode.annieq.day16;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.adventOfCode.annieq.utilities.InputImport;
+
 public class Part1 {
 
+    public static void main(String[] args) throws IOException {
+        InputImport in = new InputImport("data//day16.txt");
+        String[] moves = in.getSingleLineData().split(",");
+        
+        StringBuilder programs = new StringBuilder("abcdefghijklmnop");
+        
+        for (String move : moves) {
+            String moveType = move.substring(0, 1);
+            switch (moveType) {
+            case "s":   // spin
+                int noOfPrograms = Integer.parseInt(move.substring(1));
+                String programsToMove = programs.substring(programs.length() - noOfPrograms);
+                programs.delete(programs.length() - noOfPrograms, programs.length());
+                programs.insert(0, programsToMove);
+                break;
+            case "x":   // exchange
+                int[] positions = Arrays.asList(move.substring(1).split("/")).stream().mapToInt(Integer::parseInt).toArray();
+                char tmp = programs.charAt(positions[0]);
+                programs.setCharAt(positions[0], programs.charAt(positions[1]));
+                programs.setCharAt(positions[1], tmp);
+                break;
+            case "p":   // partner
+                String[] prgsToSwap = move.substring(1).split("/");
+                int idx1 = programs.indexOf(prgsToSwap[0]);
+                int idx2 = programs.indexOf(prgsToSwap[1]);
+                programs.setCharAt(idx1, prgsToSwap[1].charAt(0));
+                programs.setCharAt(idx2, prgsToSwap[0].charAt(0));
+                break;
+            }
+        }
+        System.out.println(programs.toString());
+    }
 }
