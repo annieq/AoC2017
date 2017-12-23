a18006955cdcd31bedf143ef407ea7bc9cb98ec2
ASzoltysek <A@ania.home>
2017-12-23 23:08:32 +0100

    day11 part1 in progress

--- src/com/adventOfCode/annieq/day11/Part1.java
+++ src/com/adventOfCode/annieq/day11/Part1.java
@@ -7,7 +7,7 @@ import com.adventOfCode.annieq.utilities.InputImport;
 public class Part1 {
 
     public static void main(String[] args) throws IOException {
-        InputImport in = new InputImport("data//day11.txt");
+        InputImport in = new InputImport("data//day11t.txt");
         String[] moves = in.getSingleLineData().split(",");
         
         // number of steps in directions:
@@ -37,8 +37,41 @@ public class Part1 {
                 break;
             }
         }
-        if (ns >= nwse && ns >= nesw) {
+        int max = Math.max(Math.abs(ns),
+                  Math.max(Math.abs(nesw), 
+                           Math.abs(nwse)));
+        int result = 0;
+        int tmp = 0;
+        if (Math.abs(ns) == max) {
             
         }
+        if (ns > 0) {
+            tmp = ns + nesw + nwse;
+            if (tmp > result)
+                result = tmp;
+        } else {
+            tmp = (ns - nesw - nwse);
+            if (tmp > result)
+                result = tmp;
+        }
+        if (nesw > 0) {
+            tmp = nesw + ns - nwse;
+            if (tmp > result)
+                result = tmp;
+        } else {
+            tmp = (nesw - ns + nwse);
+            if (tmp > result)
+                result = tmp;
+        }
+        if (nwse > 0) {
+            tmp = nwse + ns - nesw;
+            if (tmp > result)
+                result = tmp;
+        } else {
+            tmp = (nwse - ns + nesw);
+            if (tmp > result)
+                result = tmp;
+        }
+        System.out.println(result);
     }
 }
