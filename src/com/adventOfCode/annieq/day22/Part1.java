3a183c5774b6e62c37eeabd4a4c7151584cfdcc6
ASzoltysek <A@ania.home>
2017-12-23 23:20:04 +0100

    day22 started

---
+++ src/com/adventOfCode/annieq/day22/Part1.java
@@ -0,0 +1,14 @@
+package com.adventOfCode.annieq.day22;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.adventOfCode.annieq.utilities.InputImport;
+
+public class Part1 {
+
+    public static void main(String[] args) throws IOException {
+        InputImport in = new InputImport("data//day22.txt");
+        ArrayList<String> lines = in.getMultiLineData();
+    }
+}
